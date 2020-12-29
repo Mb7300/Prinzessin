d2d6eef20e887da440ad98af69ccd5991e9ca7ef
Maurizio Burri <maurizio.burri@devmentor.ch>
2020-12-29 12:23:51 +0100

    Loesung aber ausbaufähig

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Point m = new Point();
+            Point p = new Point();
+
             for (int y = 0; y < 3; y++) 
             {
                 string zeile = Console.ReadLine();
@@ -18,16 +22,17 @@ namespace ConsoleApp1
                     char zeichen = zeile[x];
                     if (zeichen  == 'm')
                     {
+                        m = new Point(x, y);
                         //Ort von m gefunden 
 
                     } 
                     else if (zeichen == 'p')
                     {
-
+                        p = new Point(x, y);
                         // Ort von p gefunden
 
                     }
-                    else if (zeichen!= '-')
+                    else if (zeichen != '-')
                     {
                         Console.WriteLine("Ungültige Zeichen vorhanden, bitte Zeile erneut eingeben!");
                         y--;
@@ -35,6 +40,41 @@ namespace ConsoleApp1
                     }
                 }
             }
+            Console.WriteLine("M ist an der Stelle: " + m.ToString());
+            Console.WriteLine("P ist an der Stelle: " + p.ToString());
+            Point weg = new Point(p.X - m.X, p.Y - m.Y);
+            Console.WriteLine("Der Weg ist: " + weg.ToString());
+
+            if (weg.X<0)
+            {
+                for (int x = 0; x < -weg.X; x++)
+                {
+                    Console.WriteLine("L");
+                }
+            } 
+            else if (weg.X>0)
+            {
+                for (int x = 0; x < weg.X; x++)
+                {
+                    Console.WriteLine("R");
+                }
+            }
+            if (weg.Y < 0)
+            {
+                for (int y = 0; y < -weg.Y; y++)
+                {
+                    Console.WriteLine("U");
+                }
+            }
+            else if (weg.Y > 0)
+            {
+                for (int y = 0; y < weg.Y; y++)
+                {
+                    Console.WriteLine("D");
+                }
+            }
+            Console.ReadKey();
+           
         }
     }
 }
